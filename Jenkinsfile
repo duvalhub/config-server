592ad4251662b0285776ef7defeaa4e204996470
Maxthod <47720660+Maxthod@users.noreply.github.com>
2021-04-14 23:50:26 -0400

    Improvement/pipeline onboarding (#1)
    * "improvement/pipeline-onboarding"

---
+++ Jenkinsfile
@@ -0,0 +1,5 @@
+@Library([
+    'shared-library@master',
+    'continuous-deployment-library@master'
+]) _
+continuousDeployment()
--- src/test/java/com/duvalhub/configserver/ApplicationTests.java
+++ src/test/java/com/duvalhub/configserver/ApplicationTests.java
@@ -2,9 +2,11 @@ package com.duvalhub.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+@ActiveProfiles("test")
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
